590a7dc6ba19a102b540b800b7a0f93a75212bd8
jhenifer oliveira <42181112+jheniferoliveira@users.noreply.github.com>
2019-09-23 22:11:26 -0300

    Add files via upload

---
+++ lista-de-carros.php
@@ -0,0 +1,7 @@
+<?php
+
+$carros = array("Placa: " => "LMS-2312", "Marca: " => "Ford", "modelo" => "fiesta");
+
+echo "Placa do carro: " .$carros["Placa: "]."<br>";
+echo "Marca do carro: " .$carros["Marca: "]."<br>";
+echo "Modelo do carro: " .$carros["Modelo: "]."<br>";
